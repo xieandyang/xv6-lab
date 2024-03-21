ee0f51f6fb2fca843f11d80a805d5024dee19414
Hongxing Xie <2018888789@qq.com>
2024-03-21 20:51:56 +0800

    lazy

--- kernel/trap.c
+++ kernel/trap.c
@@ -68,9 +68,18 @@ usertrap(void)
   } else if((which_dev = devintr()) != 0){
     // ok
   } else {
-    printf("usertrap(): unexpected scause %p pid=%d\n", r_scause(), p->pid);
-    printf("            sepc=%p stval=%p\n", r_sepc(), r_stval());
-    p->killed = 1;
+    uint64 va = r_stval();
+    if((r_scause() == 13 || r_scause() == 15) && uvmshouldtouch(va))
+    {
+      // 缺页异常，并且发生过异常的地址进行过懒分配
+      uvmlazytouch(va); // 分配物理内存，并在页表创建映射
+    }
+    else
+    {
+      printf("usertrap(): unexpected scause %p pid=%d\n", r_scause(), p->pid);
+      printf("            sepc=%p stval=%p\n", r_sepc(), r_stval());
+      p->killed = 1;
+    }
   }
 
   if(p->killed)
